d9ea4f501defc7c45f828332912f117947c6d0f9
Artem Petselia <mrtimego@ukr.net>
2020-10-03 09:40:39 +0300

    task1 done with gitignore

---
+++ tasks/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, my name is Artem Petselia!");
+            WriteLine("I'm from KP-01");
+            Console.WriteLine("Works too");
+            WriteLine(333);
+            Write(",");
+            WriteLine(3.1234476125);
+        }
+    }
+}
